5c0587ee97253fea81bbf4269b4f76d606e6d5cd
droryair <drorya2324@gmail.com>
2025-02-24 16:04:43 +0200

    created weather app

---
+++ weather-app/index.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
+
+console.log(API_KEY)
+
+app.get('/weather', async (req, res) => {
+  const city = req.query.city || 'London';
+  try {
+    const response = await axios.get(`${BASE_URL}?q=${city}&appid=${API_KEY}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching weather data');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
