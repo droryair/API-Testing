57e2c3d0116c2d9197364c46542e2e372455711f
droryair <drorya2324@gmail.com>
2025-02-24 18:19:14 +0200

    created API testing

---
+++ python-tests/test_weather.py
@@ -0,0 +1,47 @@
+import unittest
+import requests
+import requests_mock
+
+class WeatherAPITestCase(unittest.TestCase):
+    def setUp(self):
+        # Sending requests to the port the weather-app is running on
+        self.base_url = 'http://localhost:3000/weather'
+
+    @requests_mock.Mocker()
+    def test_get_weather(self, mock):
+        """
+        Testing how the weather-app behaves when given a valid city name and weather information.
+        """
+        city = 'London'
+        mock.get(f'{self.base_url}?city={city}',
+                 json={'weather': [{'description': 'clear sky'}], 'main': {'temp': 293.15}})
+
+        response = requests.get(f'{self.base_url}?city={city}')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('clear sky', response.json()['weather'][0]['description'])
+        self.assertEqual(293.15, response.json()['main']['temp'])
+
+
+    def test_get_weather_default(self):
+        """
+        Testing how the app behaves when given an empty city name.
+        """
+        city=''
+        response = requests.get(f'{self.base_url}?city={city}')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Jerusalem', response.json()['name'])
+
+    @requests_mock.Mocker()
+    def test_get_weather_error(self, mock):
+        """
+        Testing how the weather-app behaves when given a invalid city name.
+        """
+        city = 'InvalidCity'
+        mock.get(f'{self.base_url}?city={city}',
+                 status_code=500)
+
+        response = requests.get(f'{self.base_url}?city={city}')
+        self.assertEqual(response.status_code, 500)
+
+if __name__ == '__main__':
+    unittest.main()
--- weather-app/index.js
+++ weather-app/index.js
@@ -10,7 +10,7 @@ const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
 console.log(API_KEY)
 
 app.get('/weather', async (req, res) => {
-  const city = req.query.city || 'London';
+  const city = req.query.city || 'Jerusalem';
   try {
     const response = await axios.get(`${BASE_URL}?q=${city}&appid=${API_KEY}`);
     res.json(response.data);
